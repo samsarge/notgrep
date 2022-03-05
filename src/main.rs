6a07b0528a9ad820e7910fe6b96ff13bf45de2c1
Sam Sargent <samsarge@hotmail.co.uk>
2022-03-05 22:34:59 +0000

    print errors to stderr not stdout

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config: Config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem parsing arguments: {}", err); // print it so we dont get the extra stuff printed from panic!
+        eprintln!("Problem parsing arguments: {}", err); // print it so we dont get the extra stuff printed from panic!
         process::exit(1); // status 1 is program exited with error
     });
 
     if let Err(e) = notgrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
