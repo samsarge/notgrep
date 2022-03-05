d4323b6caf8137ddf3308d7bb4627c2eebeca39b
Sam Sargent <samsarge@hotmail.co.uk>
2022-03-05 22:31:37 +0000

    Add case insensitive option with environment variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fs;
 use std::error::Error;
 
@@ -5,7 +6,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?; // the ? will return the Err immediately
 
     // search returns a vec of str slices
-    for line in search(&config.text_to_search, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.text_to_search, &contents)
+    } else {
+        search_case_insensitive(&config.text_to_search, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -14,7 +21,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 pub struct Config {
     text_to_search: String,
-    filename: String
+    filename: String,
+    case_sensitive: bool
 }
 
 impl Config {
@@ -23,7 +31,12 @@ impl Config {
             return Err("Not enough arguments.");
         }
 
-        Ok(Config { text_to_search: args[1].clone(), filename: args[2].clone() })
+        let text_to_search: String = args[1].clone();
+        let filename: String = args[2].clone();
+        // will return the Err variant of Result if the env var isnt set
+        let case_sensitive: bool = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { text_to_search, filename, case_sensitive })
     }
 }
 
@@ -39,12 +52,27 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     matches
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // shadow the query variable, to_lowercase() returns a new String; not a &str.
+    let query = query.to_lowercase();
+    let mut matches = Vec::new();
+
+    for line in contents.lines() {
+        // contains needs &str so pass a borrow of query
+        if line.to_lowercase().contains(&query) {
+            matches.push(line);
+        }
+    }
+
+    matches
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "for";
         let contents = "\
 This
@@ -52,5 +80,20 @@ is a test string
 for the tests";
 
         assert_eq!(vec!["for the tests"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+
+        let query = "tEsT";
+        let contents = "\
+This
+is a TEST string
+for the tests";
+
+        assert_eq!(
+            vec!["is a TEST string", "for the tests"],
+            search_case_insensitive(query, contents)
+        );
     }
 }
